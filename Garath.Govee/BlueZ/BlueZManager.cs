8785d54c8f7c22373740189d8f7bdde5fe1f4c5e
Michael Stuckey <michael.stuckey@gmail.com>
2022-04-01 23:50:31 -0700

    Bring Hashtagchris.DotNet-BlueZ local and regenerate with Tmds.DBus 0.10.1

---
+++ Garath.Govee/BlueZ/BlueZManager.cs
@@ -0,0 +1,87 @@
+﻿using Tmds.DBus;
+
+namespace HashtagChris.DotNetBlueZ;
+
+public static class BlueZManager
+{
+    public static async Task<Adapter> GetAdapterAsync(string adapterName)
+    {
+        var adapterObjectPath = $"/org/bluez/{adapterName}";
+        var adapter = Connection.System.CreateProxy<IAdapter1>(BluezConstants.DbusService, adapterObjectPath);
+
+        try
+        {
+            await adapter.GetAliasAsync();
+        }
+        catch (Exception)
+        {
+            throw new Exception($"Bluetooth adapter {adapterName} not found.");
+        }
+
+        return await Adapter.CreateAsync(adapter);
+    }
+
+    public static async Task<IReadOnlyList<Adapter>> GetAdaptersAsync()
+    {
+        var adapters = await GetProxiesAsync<IAdapter1>(BluezConstants.AdapterInterface, rootObject: null);
+
+        return await Task.WhenAll(adapters.Select(Adapter.CreateAsync));
+    }
+
+    // Normalize a 16, 32 or 128 bit UUID.
+    public static string NormalizeUUID(string uuid)
+    {
+        // TODO: Improve this validation.
+        if (uuid.Length == 4)
+        {
+            return $"0000{uuid}-0000-1000-8000-00805f9b34fb".ToLowerInvariant();
+        }
+        else if (uuid.Length == 8)
+        {
+            return $"{uuid}-0000-1000-8000-00805f9b34fb".ToLowerInvariant();
+        }
+        else if (uuid.Length == 36)
+        {
+            return uuid.ToLowerInvariant();
+        }
+        else
+        {
+            throw new ArgumentException($"'{uuid}' isn't a valid 16, 32 or 128 bit UUID.");
+        }
+    }
+
+    /// <param name="interfaceName">The interface to search for</param>
+    /// <param name="rootObject">The DBus object to search under. Can be null</param>
+    internal static async Task<IReadOnlyList<T>> GetProxiesAsync<T>(string interfaceName, IDBusObject rootObject)
+    {
+        // Console.WriteLine("GetProxiesAsync called.");
+        var objectManager = Connection.System.CreateProxy<IObjectManager>(BluezConstants.DbusService, "/");
+        var objects = await objectManager.GetManagedObjectsAsync();
+
+        var matchingObjectPaths = objects
+            .Where(obj => IsMatch(interfaceName, obj.Key, obj.Value, rootObject))
+            .Select(obj => obj.Key);
+
+        var proxies = matchingObjectPaths
+            .Select(objectPath => Connection.System.CreateProxy<T>(BluezConstants.DbusService, objectPath))
+            .ToList();
+
+        // Console.WriteLine($"GetProxiesAsync returning {proxies.Count} proxies of type {typeof(T)}.");
+        return proxies;
+    }
+
+    internal static bool IsMatch(string interfaceName, ObjectPath objectPath, IDictionary<string, IDictionary<string, object>> interfaces, IDBusObject rootObject)
+    {
+        return IsMatch(interfaceName, objectPath, interfaces.Keys, rootObject);
+    }
+
+    internal static bool IsMatch(string interfaceName, ObjectPath objectPath, ICollection<string> interfaces, IDBusObject rootObject)
+    {
+        if (rootObject != null && !objectPath.ToString().StartsWith($"{rootObject.ObjectPath}/"))
+        {
+            return false;
+        }
+
+        return interfaces.Contains(interfaceName);
+    }
+}
---
+++ Garath.Govee/BlueZ/Extensions.cs
@@ -0,0 +1,113 @@
+﻿using Tmds.DBus;
+
+namespace HashtagChris.DotNetBlueZ.Extensions;
+
+public static class Extensions
+{
+    public static async Task<IReadOnlyList<Device>> GetDevicesAsync(this IAdapter1 adapter)
+    {
+        var devices = await BlueZManager.GetProxiesAsync<IDevice1>(BluezConstants.DeviceInterface, adapter);
+
+        return await Task.WhenAll(devices.Select(Device.CreateAsync));
+    }
+
+    public static async Task<Device> GetDeviceAsync(this IAdapter1 adapter, string deviceAddress)
+    {
+        var devices = await BlueZManager.GetProxiesAsync<IDevice1>(BluezConstants.DeviceInterface, adapter);
+
+        var matches = new List<IDevice1>();
+        foreach (var device in devices)
+        {
+            if (String.Equals(await device.GetAddressAsync(), deviceAddress, StringComparison.OrdinalIgnoreCase))
+            {
+                matches.Add(device);
+            }
+        }
+
+        // BlueZ can get in a weird state, probably due to random public BLE addresses.
+        if (matches.Count > 1)
+        {
+            throw new Exception($"{matches.Count} devices found with the address {deviceAddress}!");
+        }
+
+        var dev = matches.FirstOrDefault();
+        if (dev != null)
+        {
+            return await Device.CreateAsync(dev);
+        }
+        return null;
+    }
+
+    public static Task<IDisposable> WatchDevicesAddedAsync(this IAdapter1 adapter, Action<Device> handler)
+    {
+        async void OnDeviceAdded((ObjectPath objectPath, IDictionary<string, IDictionary<string, object>> interfaces) args)
+        {
+            if (BlueZManager.IsMatch(BluezConstants.DeviceInterface, args.objectPath, args.interfaces, adapter))
+            {
+                var device = Connection.System.CreateProxy<IDevice1>(BluezConstants.DbusService, args.objectPath);
+
+                var dev = await Device.CreateAsync(device);
+                handler(dev);
+            }
+        }
+
+        var objectManager = Connection.System.CreateProxy<IObjectManager>(BluezConstants.DbusService, "/");
+        return objectManager.WatchInterfacesAddedAsync(OnDeviceAdded);
+    }
+
+
+    // TODO: Make this available to other generated interfaces too, not just IDevice1.
+    // `dynamic obj` works, but it requires a Microsoft.* NuGet package and isn't type safe.
+    public static async Task WaitForPropertyValueAsync<T>(this IDevice1 obj, string propertyName, T value, TimeSpan timeout)
+    {
+        var (watchTask, watcher) = WaitForPropertyValueInternal<T>(obj, propertyName, value);
+        var currentValue = await obj.GetAsync<T>(propertyName);
+        // Console.WriteLine($"{propertyName}: {currentValue}");
+
+        // https://stackoverflow.com/questions/390900/cant-operator-be-applied-to-generic-types-in-c
+        if (EqualityComparer<T>.Default.Equals(currentValue, value))
+        {
+            watcher.Dispose();
+            return;
+        }
+
+        await Task.WhenAny(new Task[] { watchTask, Task.Delay(timeout) });
+        if (!watchTask.IsCompleted)
+        {
+            throw new TimeoutException($"Timed out waiting for '{propertyName}' to change to '{value}'.");
+        }
+
+        // propogate any exceptions.
+        await watchTask;
+    }
+
+    private static (Task, IDisposable) WaitForPropertyValueInternal<T>(IDevice1 obj, string propertyName, T value)
+    {
+        var taskSource = new TaskCompletionSource<bool>();
+
+        IDisposable watcher = null;
+        watcher = obj.WatchPropertiesAsync(propertyChanges => {
+            try
+            {
+                if (propertyChanges.Changed.Any(kvp => kvp.Key == propertyName))
+                {
+                    var pair = propertyChanges.Changed.Single(kvp => kvp.Key == propertyName);
+                    if (pair.Value.Equals(value))
+                    {
+                        // Console.WriteLine($"[CHG] {propertyName}: {pair.Value}.");
+                        taskSource.SetResult(true);
+                        watcher.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex}");
+                taskSource.SetException(ex);
+                watcher.Dispose();
+            }
+        });
+
+        return (taskSource.Task, watcher);
+    }
+}
